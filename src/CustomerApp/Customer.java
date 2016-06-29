753c939a0462976afbbf2fc6f9b7d120745f8e44
Josh T <jptucker2009@gmail.com>
2016-06-29 12:03:03 -0400

    Second commit

--- src/CustomerApp/Customer.java
+++ src/CustomerApp/Customer.java
@@ -56,18 +56,72 @@ public class Customer {
 		}
 
 	}
-	public static void updateCustomerData(String FirstName,String LastName,String EmailAdress, String NewStreet,
-			String NewCity,String NewZip,String NewState) {
+	public static void updateCustomerData(String FirstName,String LastName,String EmailAddress, String NewStreet
+			) {
+		//String NewCity,String NewZip,String NewState
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		String lastName = LastName;
 		String firstName=FirstName;
-		String email=EmailAdress;
-		String streetAdress=NewStreet;
+		String email=EmailAddress;
+		String streetAddress=NewStreet;
 		
-		String sql = "update  customers set streetadress="+"\'"+streetAdress+"\'"
+		String sql = "update  customers set streetaddress="+"\'"+streetAddress+"\'"
 				+ " where lastname=" +"\'" + lastName + "\'"+" and firstname="+"\'"+firstName+"\'";
+		
+		
+		
+		String printStatement = "Still not working";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// con = DriverManager.getConnection("jdbc:oracle:thin:sys as
+			// sysdba/oracle@localhost:1521:orcl");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			//while (rs.next()) {
+				// printStatement= rs.getString(1);
+			//	System.out.println("\n"+"Customer: " + rs.getString("Customerid"));
+			//	System.out.println(rs.getString("Title") + rs.getString("Firstname") + " " + rs.getString("lastname"));
+			//	System.out.println(rs.getString("Streetaddress") + "\n" + rs.getString("zipcode")
+			//	+ rs.getString("cityid") + " " + rs.getString("stateid"));
+			//	System.out.println(rs.getString("emailaddress"));
+			//	System.out.println(rs.getString("positionid")+" at "+rs.getString("companyid"));
+
+			//}
+		} catch (SQLException e) {
+			System.err.println("You have enetered an invalid sql statement");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Data you are searching for is unavailable");
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	public static void addNewCustomer(String FirstName,String LastName,String EmailAddress, 
+			String StreetAddress) {
+		//String NewCity,String NewZip,String NewState
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String lastName = LastName;
+		String firstName=FirstName;
+		String email=EmailAddress;
+		String streetAddress=StreetAddress;
+		
+		String sql = "insert into  customers(firstname,lastname,streetaddress,"
+				+ "emailadress) values(\'"+firstName+"\',"
+				+"\'" + lastName + "\',"+"\'"+streetAddress+"\'";
 		
 		
 		
--- src/CustomerApp/RunCustomerApp.java
+++ src/CustomerApp/RunCustomerApp.java
@@ -6,32 +6,43 @@ public class RunCustomerApp {
 		// TODO Auto-generated method stub
 		Scanner in= new Scanner(System.in);
 		
-
-		while(true){
-			System.out.println("Enter (1) to search customers by last name, (2) update customer information"
+System.out.println("Enter (1) to search customers by last name, (2) update customer information"
 					+ "(3) add new customer, (4) to quit");
+		while(true){
+			
 			int a=in.nextInt();
-		
 		if(a==1){
 		System.out.println("Enter customers last name");
-		String lastname=in.next();
+		String lastname=in.nextLine();
 		Customer.searchByLastName(lastname);
 		}else if(a==2){
 			System.out.println("Enter customers first name");
-			String firstname=in.next();
+			String firstname=in.nextLine();
 			System.out.println("Enter customers last name");
-			String lastname=in.next();
-			System.out.println("Enter customers email adress");
-			String emailadress=in.next();
-			
+			String lastname=in.nextLine();
+			System.out.println("Enter customers email address");
+			String emailaddress=in.nextLine();
+			System.out.println("Enter customers new street address");
+			String newaddress=in.nextLine();
+			Customer.updateCustomerData(firstname, lastname, emailaddress, newaddress);
 		}else if(a==3){
-			
+			System.out.println("Enter customers first name");
+			String firstname=in.nextLine();
+			System.out.println("Enter customers last name");
+			String lastname=in.nextLine();
+			System.out.println("Enter customers email address");
+			String emailaddress=in.nextLine();
+			System.out.println("Enter customers street address");
+			String streetaddress=in.nextLine();
+			Customer.addNewCustomer(firstname, lastname, emailaddress, streetaddress);
 		}else if(a==4){
 			break;
 		}else{
 			System.err.println("An error has occured."
-					+ "You were not supposed to reach this point");
+					+ "You entered an invalid number");
+			break;
 		}
+		System.out.println();
 	}
 }
 }
